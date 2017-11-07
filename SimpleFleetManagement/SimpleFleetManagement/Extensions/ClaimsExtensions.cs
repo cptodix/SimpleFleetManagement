ffb0eec502d3e0e19a3c39c9fb81c321ead76912
cptodix <cptodix@gmail.com>
2017-11-07 07:28:51 +0700

    Basic Layout

--- SimpleFleetManagement/SimpleFleetManagement/Extensions/ClaimsExtensions.cs
+++ SimpleFleetManagement/SimpleFleetManagement/Extensions/ClaimsExtensions.cs
@@ -2,7 +2,7 @@
 using System.Security.Claims;
 using System.Security.Principal;
 
-namespace MVC05.MVC.Extensions
+namespace SimpleFleetManagement.Extensions
 {
     public static class ClaimsExtensions
     {
