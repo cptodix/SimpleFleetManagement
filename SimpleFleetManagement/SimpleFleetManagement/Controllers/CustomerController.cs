0d4d1a37535f115c70f4c303cccf6764b39df295
cptodix <cptodix@gmail.com>
2017-11-08 13:45:22 +0700

    restart .gitignore

---
+++ SimpleFleetManagement/SimpleFleetManagement/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleFleetManagement.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            List<CrewViewModel> model = CrewDataAccess.GetAll();
+            return View(model);
+        }
+    }
+}
